a115827899f22636cc6778f1c522fc709e1df247
felixp133 <pinofelipe110@gmail.com>
2023-06-06 12:44:49 -0400

    commit

--- Retos/reto7.py
+++ Retos/reto7.py
@@ -1,21 +1,13 @@
-frase=input(str("Ingrese una frase: "))
-def funcion1 ():
+ 
+def leerfrase ():
+    frase=input(str("Ingrese una frase: "))
+    separacion=frase.split()
+        
+    diccionario={}
 
-    quitar=",.;:\n!\""
-    for caracter in quitar:           
-        frase=frase.replace(caracter,
-                    "")   
-    
-    frase=frase.lower()
-    palabras=frase.split() 
-    v1=frase.count
-    lfrase=len(frase)
-    diccionario={
-        "cantidad de palabras ":v1,
-        "Longitud de frase":lfrase ,
+    for i in separacion:
+        diccionario[i]=len(i)
 
-    }
     print(diccionario)
 
-
-print(funcion1)
+leerfrase()
