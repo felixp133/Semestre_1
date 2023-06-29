02cfc3906d1ce0b59ba0ab3f802b58efff8d9cc1
felixp133 <pinofelipe110@gmail.com>
2023-06-28 21:29:35 -0400

    confirm

---
+++ Control/Lab4-ej1.py
@@ -0,0 +1,29 @@
+pacientes = [ ["Pedro", 1.78], ["Constanza", 1.56], ["Amanda", 1.62] , ["Dario", 1.89],
+["Fernanda", 1.67] ]
+print(pacientes[3])
+#A)
+def estatura_minima(pacientes):
+    estmin=float(min(pacientes)) 
+    print(estmin)
+estatura_minima
+#print(estatura_minima)
+#B)
+Npaciente=["Ricardo",[1.71]]
+def nuevo_pciente (pacientes):
+    pacientes2=pacientes+Npaciente
+    return pacientes2 
+print (nuevo_pciente)
+    
+#C)
+def encuentra_dario(pacientes2):
+    for i in pacientes2 :
+        if i in "Dario":
+            print("El paciente dario si se encentra en la lista")
+            print(pacientes2[3])
+        else:
+            print("El paciente dario no se encentra en la lista")
+
+#D)
+estatura_minima
+nuevo_pciente
+encuentra_dario
